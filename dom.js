190efe045946d6b0dae2670281ed14ade7d6d793
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-18 13:22:50 +0600

    recap of DOM

--- dom.js
+++ dom.js
@@ -3,6 +3,25 @@ document.getElementById('apply-bg').addEventListener('click',function(){
     const friends = document.getElementsByClassName('friend');
     // console.log(friends);
    for(const friend of friends){
-    console.log(friend);
+    // console.log(friend);
+    friend.style.backgroundColor ='cyan';
    };
+})
+
+document.getElementById('center-third').addEventListener('click',function(){
+    // console.log('centering');
+    const third = document.getElementById('third-friend');
+    third.style.textAlign = 'center';
+})
+
+document.getElementById('add-friend').addEventListener('click',function(){
+    // console.log('adding');
+    const friendContainer = document.getElementById('friends');
+    const friend = document.createElement('div');
+    friend.classList.add('friend');
+    friend.innerHTML = `
+    <h3 class = 'friend-name'> New Friend </h3>
+    <p>Something new addeded</p>
+    `
+    friendContainer.appendChild(friend);
 })
