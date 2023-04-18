1d27972b2c1d6877f65de03b46cb4881d7fba724
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-18 17:30:41 +0600

    backtick

---
+++ backtick.js
@@ -0,0 +1,13 @@
+// string a multiline lekhar jonnoo backtick use kora hoi
+const multiline = `amar sonar bangla 
+ami tomai valobasi
+chirodin tomar akas
+tomar batas`;
+console.log(multiline);
+
+// backtick use korar r akta subidha holo ${ } ata.ata bosale kono akta variable er man sorasori bosabe.
+// `${}` ata use korle barbar + use korar dorkar hoina.
+const a = 21;
+const b = 12;
+const newSummery = `sum of ${a} and ${b} is:${a+b}`;
+console.log(newSummery);
