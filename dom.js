f16dc55707b010edcff7178f747c5b53d0a4dedf
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-18 12:53:14 +0600

    ES6

---
+++ dom.js
@@ -0,0 +1,8 @@
+document.getElementById('apply-bg').addEventListener('click',function(){
+    // console.log('apply background');
+    const friends = document.getElementsByClassName('friend');
+    // console.log(friends);
+   for(const friend of friends){
+    console.log(friend);
+   };
+})
