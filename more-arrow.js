f4ae207aa03245d6fb86adbe8fa64cc3c3c5f45e
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-19 11:11:05 +0600

    more arrow function and big arrow function

---
+++ more-arrow.js
@@ -0,0 +1,31 @@
+const add = (first , second) => first + second;
+const getFullName = (first , last) => first + '' + last;
+const multiply = (a,b) => a*b;
+
+const result = multiply(4,3);
+console.log(result);
+
+// no parameter arrow function
+const getPie = () => 3.14;
+
+// one parameter 
+const doubleIt = (num) => num*2;
+
+// one parameter simple version 
+const fiveTimes = num =>num*5;
+
+const doMath = (x,y,z) =>{
+    const firstSum = x+y;
+    const secondSum = y+z;
+    const multiplyResult = firstSum * secondSum;
+    const result = multiplyResult/2;
+    return result;
+}
+
+// ES6
+let add = (x,y) => x+y;
+
+// ES5
+function add(x,y){
+    return x+y;
+}
