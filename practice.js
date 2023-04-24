b69b9b074cbfed3c40e04bd4efd60d39eceb37ef
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-24 23:34:30 +0600

    defferent line and default perameter

---
+++ practice.js
@@ -0,0 +1,15 @@
+
+
+// problem 1 :
+const parameters = (x,y,z) =>x*y*z;
+console.log(parameters(3,4,5));
+
+// problem 2 ( defferent line):
+const threeLineSentence = `Iam a web developer
+I love to code
+I love to eat biriyani`;
+console.log(threeLineSentence);
+
+// problem 3 (default perameter):
+var fullName = (first , last = 'Akter') => first + ' ' + last;
+console.log(fullName('Rinky'));
