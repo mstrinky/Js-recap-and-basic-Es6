af2fb5923e32b1eaa1b9de609df9f1d5bde65b14
Mst Rinky Akter <mstrinkya@gmail.com>
2023-05-14 00:02:08 +0600

    set

---
+++ problem.js
@@ -0,0 +1,20 @@
+// const friendsName = [];
+// const name = new Array();
+// const mySet = new Set();
+// mySet.add(1);
+// mySet.add('rinky');
+// mySet.delete('rinky');
+
+// mySet.add(2);
+// console.log(mySet);
+
+
+const food = [1,2,3,3,2,45,5,6];
+console.log(food);
+const mySet = new Set(food);
+mySet.add(25);
+mySet.add(25);
+mySet.delete(25);
+console.log(mySet);
+
+
